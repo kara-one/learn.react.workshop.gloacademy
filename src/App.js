0f9efc468bda701141f368d266cf9b851ee93a31
Serhii Karavashkin <ksawl@narod.ru>
2020-10-13 22:31:40 +0300

    Day 1 / Homework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './style.css';
 
+import Calendar from './components/Calendar/Calendar';
+import Details from './components/Details/Details';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import React from 'react';
@@ -9,6 +13,10 @@ function App() {
         <>
             <Header />
             <Main />
+            <Features />
+            <Details />
+            <Calendar />
+            <Footer />
         </>
     );
 }
